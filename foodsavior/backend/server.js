e86b4ceca31776fc1ea92568b9622587bc91d29d
Soumyadeepa Nayek <168195852+ritu-student@users.noreply.github.com>
2025-08-14 21:22:07 +0530

    Your commit message

--- foodsavior/backend/server.js
+++ foodsavior/backend/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/userRoutes');
-const foodPostRoutes = require('./routes/foodPostRoutes'); // ✅ Import your FoodPost routes
+const foodPostRoutes = require('./routes/foodPostRoutes'); 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// ✅ Middleware to parse JSON
+// Middleware to parse JSON
 app.use(express.json());
 
-// ✅ Routes
+//  Routes
 app.use('/api/users',require('./routes/userRoutes'));
-app.use('/api/foodposts', foodPostRoutes); // ✅ Register FoodPost routes
+app.use('/api/foodposts', foodPostRoutes); //  Register FoodPost routes
+app.get("/", (req, res) => {
+  res.send("FoodSavior API is running...");
+});
 
-// ✅ DB Connection
+//  DB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
